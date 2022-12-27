7ef9961f9ea03578243e2d6ba276629a628143c9
unknown <sejong5.kr@gmail.com>
2022-12-27 12:27:38 +0900

    first commit
    for Phthon training

---
+++ hellopithon.py
@@ -0,0 +1,14 @@
+movies = ["The Holy Grail", "The Life of Brian", "The Meaning of Life"]
+print(movies[1])
+
+cast=["cheese", "bread", "milk"]
+cast.extend(["sugar", "cinamon", "cocoa", "butter"])
+print(cast)
+num1 = 0
+for ei in cast : 
+    print(num1, ':', ei)
+    num1=num1+1
+
+
+
+
